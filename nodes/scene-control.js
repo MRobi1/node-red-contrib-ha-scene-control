b5ce9f1d8a8e4232354fc78ab1c1b1be37844635
Ryan Johnson <ryanjohnson95@gmail.com>
2021-05-13 15:24:35 -0500

    Test Push

--- nodes/scene-control.js
+++ nodes/scene-control.js
@@ -1,158 +1,380 @@
-<script type="text/javascript">
-  RED.nodes.registerType("scene-control", {
-    category: "home assistant",
-    color: "#B68181",
-    defaults: {
-      name: { value: "" },
-      nodeid: { value: "" },
-      zwave: { value: "zwave_js" },
-      switchtype: { value: "LZW30" },
-      outputs: { value: 1 },
-      labels: { value: ["Please select a Switch Type"] },
-    },
-    inputs: 1,
-    outputs: 1,
-    inputLabels: "Connect to an events-all node",
-    outputLabels: function (index) {
-      return this.labels[index];
-    },
-    icon: "light.svg",
-    align: "right",
-    label: function () {
-      return this.name || "scene-control";
-    },
-    oneditprepare: function () {},
-    oneditsave: function () {
-      const switchtype = $("#node-input-switchtype").val();
-      let label;
-      switch (switchtype) {
-        case "LZW30":
-          label = [
-            "Tap Up on Light Paddle 1x",
-            "Tap Up on Light Paddle 2x",
-            "Tap Up on Light Paddle 3x",
-            "Tap Up on Light Paddle 4x",
-            "Tap Up on Light Paddle 5x",
-            "Hold Up on Light Paddle",
-            "Release Up on Light Paddle",
-            "Tap Down on Light Paddle 1x",
-            "Tap Down on Light Paddle 2x",
-            "Tap Down on Light Paddle 3x",
-            "Tap Down on Light Paddle 4x",
-            "Tap Down on Light Paddle 5x",
-            "Hold Down on Light Paddle",
-            "Release Down on Light Paddle",
-            "Tap Config Button 1x",
-          ];
-          break;
-        case "LZW31":
-          label = [
-            "Tap Up on Light Paddle 1x",
-            "Tap Up on Light Paddle 2x",
-            "Tap Up on Light Paddle 3x",
-            "Tap Up on Light Paddle 4x",
-            "Tap Up on Light Paddle 5x",
-            "Hold Up on Light Paddle",
-            "Release Up on Light Paddle",
-            "Tap Down on Light Paddle 1x",
-            "Tap Down on Light Paddle 2x",
-            "Tap Down on Light Paddle 3x",
-            "Tap Down on Light Paddle 4x",
-            "Tap Down on Light Paddle 5x",
-            "Hold Down on Light Paddle",
-            "Release Down on Light Paddle",
-            "Tap Config Button 1x",
-          ];
-          break;
-        case "LZW36":
-          label = [
-            "Tap on Light Button 1x",
-            "Tap on Light Button 2x",
-            "Tap on Light Button 3x",
-            "Tap on Light Button 4x",
-            "Tap on Light Button 5x",
-            "Hold on Light Button (3 Seconds)",
-            "Release on Light (After Hold) Button",
-            "Tap on Light Rocker Up",
-            "Tap on Light Rocker Down",
-            "Tap on Fan Button 1x",
-            "Tap on Fan Button 2x",
-            "Tap on Fan Button 3x",
-            "Tap on Fan Button 4x",
-            "Tap on Fan Button 5x",
-            "Hold on Fan Button (3 Seconds)",
-            "Release on Fan (After Hold) Button",
-            "Tap on Fan Rocker Up",
-            "Tap on Fan Rocker Down",
-          ];
-          break;
-        case "LZW45":
-          label = [
-            "Tap Up on Light Paddle 1x",
-            "Tap Up on Light Paddle 2x",
-            "Tap Up on Light Paddle 3x",
-            "Tap Up on Light Paddle 4x",
-            "Tap Up on Light Paddle 5x",
-            "Hold Up on Light Paddle",
-            "Release Up on Light Paddle",
-            "Tap Down on Light Paddle 1x",
-            "Tap Down on Light Paddle 2x",
-            "Tap Down on Light Paddle 3x",
-            "Tap Down on Light Paddle 4x",
-            "Tap Down on Light Paddle 5x",
-            "Hold Down on Light Paddle",
-            "Release Down on Light Paddle",
-            "Tap Config Button 1x",
-          ];
-          break;
+module.exports = function (RED) {
+  function SceneControl(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    const { zwave, nodeid, switchtype, outputs } = config;
+    this.zwave = zwave;
+    this.nodeid = nodeid;
+    this.switchtype = switchtype;
+    this.outputs = parseInt(outputs, 10);
+
+    node.on("input", (msg) => {
+      const { zwave: presetZwave, nodeid, switchtype, outputs } = node;
+      const { payload } = msg;
+      const domain = payload.event.domain;
+      const event_type = payload.event_type;
+      const nodes = nodeid.split(',').map(Number);
+      const node_id = parseInt(payload.event.node_id);
+      const event_types = {
+        zwave_js: "zwave_js_value_notification",
+        ozw: "ozw.scene_activated",
+        zwave: "zwave.scene_activated",
+      };
+
+      var error = 0;
+      function validateDomain(preset, incoming) {
+        if (preset !== incoming) {
+          node.error(
+            `Incorrect Z-Wave Integration. Node is set for ${preset} but recieved an event for ${incoming}`
+          );
+          error++;
+        } else {
+          validateEventType();
+        }
       }
-      this.outputs = label.length;
-      this.labels = label;
-    },
-  });
-</script>
+      function validateEventType() {
+        if (event_types[domain] !== event_type) {
+          let correctType = event_types[domain];
+          node.error(
+            `Incorrect Event Type Recieved. Node is set for ${domain} which expects an event type of ${correctType}`
+          );
+          error++;
+        }
+      }
+      validateDomain(presetZwave, domain);
 
-<script type="text/x-red" data-template-name="scene-control">
-  <style>
-    .current-outputs {
-      display: none;
-    }
-  </style>
-  <div class="form-row">
-      <label for="node-input-name"><i class="icon-tag"></i>Name</label>
-      <input type="text" id="node-input-name" placeholder="Name">
-  </div>
-  <div class="form-row">
-    <label for="node-input-zwave"><i class="icon-tag"></i>Z-Wave Integration</label>
-    <select name="node-input-zwave" id="node-input-zwave">
-        <option value="zwave_js">Z-Wave JS</option>
-    </select>
-  </div>
-  <div class="form-row nodeid-input">
-    <label for="node-input-nodeid"><i class="icon-tag"></i>Node ID</label>
-    <input type="text" id="node-input-nodeid">
-  </div>
-  <div class="form-row">
-    <label for="node-input-switchtype"><i class="icon-tag"></i>Switch Type</label>
-    <select name="node-input-switchtype" id="node-input-switchtype">
-        <option value="LZW30">On/Off Switch (LZW30-SN)</option>
-        <option value="LZW31">Dimmer Switch (LZW31-SN)</option>
-        <option value="LZW36">Fan/Light Switch (LZW36)</option>
-        <option value="LZW45">Lightstrip & Controller Kit (LZW45)</option>
-    </select>
-  </div>
-</script>
-
-<script type="text/x-red" data-help-name="scene-control">
-  <p>This node will decipher button clicks on an Inovelli Red Series switch allowing for easy scene control in Home Assistant. Just connect this input of this node to the output of a Home Assistant "events: all" node</p>
-  <dl class="message-properties">
-      <dt>Z-Wave Integration<span class="property-type">string</span></dt>
-      <dd>Your Z-Wave integration used in Home Assistant (zwave_js, ozw or zwave)</dd>
-
-      <dt>Node ID<span class="property-type">number</span></dt>
-      <dd>Node ID (or a comma-delimited list of node IDs) of the Inovelli switch you are trying to configure</dd>
-
-      <dt>Switch Type<span class="property-type">number|string</span></dt>
-      <dd>Determines which Inovelli switch is being used. (LZW30-SN, LZW31-SN, LZW36, LZW45)</dd>
-  </dl>
-</script>
+      if (nodes.includes(node_id) === true && error === 0) {
+        const LZW30Map = {
+          0: {
+            button: 2,
+            scene: 1,
+          },
+          1: {
+            button: 2,
+            scene: 4,
+          },
+          2: {
+            button: 2,
+            scene: 5,
+          },
+          3: {
+            button: 2,
+            scene: 6,
+          },
+          4: {
+            button: 2,
+            scene: 7,
+          },
+          5: {
+            button: 2,
+            scene: 2,
+          },
+          6: {
+            button: 2,
+            scene: 3,
+          },
+          7: {
+            button: 1,
+            scene: 1,
+          },
+          8: {
+            button: 1,
+            scene: 4,
+          },
+          9: {
+            button: 1,
+            scene: 5,
+          },
+          10: {
+            button: 1,
+            scene: 6,
+          },
+          11: {
+            button: 1,
+            scene: 7,
+          },
+          12: {
+            button: 1,
+            scene: 2,
+          },
+          13: {
+            button: 1,
+            scene: 3,
+          },
+          14: {
+            button: 3,
+            scene: 1,
+          },
+        };
+        const LZW31Map = {
+          0: {
+            button: 2,
+            scene: 1,
+          },
+          1: {
+            button: 2,
+            scene: 4,
+          },
+          2: {
+            button: 2,
+            scene: 5,
+          },
+          3: {
+            button: 2,
+            scene: 6,
+          },
+          4: {
+            button: 2,
+            scene: 7,
+          },
+          5: {
+            button: 2,
+            scene: 2,
+          },
+          6: {
+            button: 2,
+            scene: 3,
+          },
+          7: {
+            button: 1,
+            scene: 1,
+          },
+          8: {
+            button: 1,
+            scene: 4,
+          },
+          9: {
+            button: 1,
+            scene: 5,
+          },
+          10: {
+            button: 1,
+            scene: 6,
+          },
+          11: {
+            button: 1,
+            scene: 7,
+          },
+          12: {
+            button: 1,
+            scene: 2,
+          },
+          13: {
+            button: 1,
+            scene: 3,
+          },
+          14: {
+            button: 3,
+            scene: 1,
+          },
+        };
+        const LZW36Map = {
+          0: {
+            button: 2,
+            scene: 0,
+          },
+          1: {
+            button: 2,
+            scene: 3,
+          },
+          2: {
+            button: 2,
+            scene: 4,
+          },
+          3: {
+            button: 2,
+            scene: 5,
+          },
+          4: {
+            button: 2,
+            scene: 6,
+          },
+          5: {
+            button: 2,
+            scene: 2,
+          },
+          6: {
+            button: 2,
+            scene: 1,
+          },
+          7: {
+            button: 3,
+            scene: 0,
+          },
+          8: {
+            button: 4,
+            scene: 0,
+          },
+          9: {
+            button: 1,
+            scene: 0,
+          },
+          10: {
+            button: 1,
+            scene: 3,
+          },
+          11: {
+            button: 1,
+            scene: 4,
+          },
+          12: {
+            button: 1,
+            scene: 5,
+          },
+          13: {
+            button: 1,
+            scene: 6,
+          },
+          14: {
+            button: 1,
+            scene: 2,
+          },
+          15: {
+            button: 1,
+            scene: 1,
+          },
+          16: {
+            button: 5,
+            scene: 0,
+          },
+          17: {
+            button: 6,
+            scene: 0,
+          },
+        };
+        const LZW45Map = {
+          0: {
+            button: 2,
+            scene: 1,
+          },
+          1: {
+            button: 2,
+            scene: 4,
+          },
+          2: {
+            button: 2,
+            scene: 5,
+          },
+          3: {
+            button: 2,
+            scene: 6,
+          },
+          4: {
+            button: 2,
+            scene: 7,
+          },
+          5: {
+            button: 2,
+            scene: 2,
+          },
+          6: {
+            button: 2,
+            scene: 3,
+          },
+          7: {
+            button: 1,
+            scene: 1,
+          },
+          8: {
+            button: 1,
+            scene: 4,
+          },
+          9: {
+            button: 1,
+            scene: 5,
+          },
+          10: {
+            button: 1,
+            scene: 6,
+          },
+          11: {
+            button: 1,
+            scene: 7,
+          },
+          12: {
+            button: 1,
+            scene: 2,
+          },
+          13: {
+            button: 1,
+            scene: 3,
+          },
+          14: {
+            button: 3,
+            scene: 1,
+          },
+        };
+        var button, scene;
+        var output = new Array(outputs);
+        switch (domain) {
+          case "zwave_js":
+            button = parseInt(payload.event.property_key);
+            scene = parseInt(payload.event.value_raw);
+            break;
+          case "ozw":
+            button = parseInt(payload.event.scene_id);
+            scene = parseInt(payload.event.scene_value_id);
+            break;
+          case "zwave":
+            button = parseInt(payload.event.scene_id);
+            var sceneConvert = {
+              7620: 0,
+              7680: 1,
+              7740: 2,
+              7800: 3,
+              7860: 4,
+              7920: 5,
+              7980: 6,
+              8040: 7,
+            };
+            scene = sceneConvert[payload.event.scene_data];
+            break;
+        }
+        switch (switchtype) {
+          case "LZW30":
+            for (var i = 0; i < outputs; i++) {
+              if (
+                button === LZW30Map[i].button &&
+                scene === LZW30Map[i].scene
+              ) {
+                output[i] = msg;
+              }
+            }
+            break;
+          case "LZW31":
+            for (var i = 0; i < outputs; i++) {
+              if (
+                button === LZW31Map[i].button &&
+                scene === LZW31Map[i].scene
+              ) {
+                output[i] = msg;
+              }
+            }
+            break;
+          case "LZW36":
+            for (var i = 0; i < outputs; i++) {
+              if (
+                button === LZW36Map[i].button &&
+                scene === LZW36Map[i].scene
+              ) {
+                output[i] = msg;
+              }
+            }
+            break;
+          case "LZW45":
+            for (var i = 0; i < outputs; i++) {
+              if (
+                button === LZW45Map[i].button &&
+                scene === LZW45Map[i].scene
+              ) {
+                output[i] = msg;
+              }
+            }
+            break;
+        }
+        node.send(output);
+      }
+    });
+  }
+  RED.nodes.registerType("scene-controlr", SceneControl);
+};
